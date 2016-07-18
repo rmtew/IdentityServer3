2f017d193946bbfb053854c9ca376db5fb614b38
richard.m.tew@gmail.com <richard.m.tew@gmail.com>
2016-07-19 10:17:06 +1200

    Cookie manager hack to test if it works.

--- source/Core/Configuration/AppBuilderExtensions/ConfigureCookieAuthenticationExtension.cs
+++ source/Core/Configuration/AppBuilderExtensions/ConfigureCookieAuthenticationExtension.cs
@@ -19,18 +19,122 @@ using IdentityServer3.Core.Configuration;
 using IdentityServer3.Core.Configuration.Hosting;
 using IdentityServer3.Core.Extensions;
 using IdentityServer3.Core.Services;
+using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.DataHandler;
 using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Owin
 {
     internal static class UseCookieAuthenticationExtension
     {
-        public static IAppBuilder ConfigureCookieAuthentication(this IAppBuilder app, CookieOptions options, IDataProtector dataProtector)
+        public interface IIdSrvCookieManager : Microsoft.Owin.Infrastructure.ICookieManager { RequestCookieCollection GetCookiesNames(Microsoft.Owin.IOwinContext context); }
+
+        public class SystemWebIdSrvCookieManager : IIdSrvCookieManager
+        {
+            public string GetRequestCookie(IOwinContext context, string key)
+            {
+                if (context == null)
+                {
+                    throw new ArgumentNullException("context");
+                }
+
+                var webContext = context.Get<HttpContextBase>(typeof(HttpContextBase).FullName);
+                var cookie = webContext.Request.Cookies[key];
+                return cookie == null ? null : cookie.Value;
+            }
+
+            public void AppendResponseCookie(IOwinContext context, string key, string value, Microsoft.Owin.CookieOptions options)
+            {
+                if (context == null)
+                {
+                    throw new ArgumentNullException("context");
+                }
+                if (options == null)
+                {
+                    throw new ArgumentNullException("options");
+                }
+
+                var webContext = context.Get<HttpContextBase>(typeof(HttpContextBase).FullName);
+
+                bool domainHasValue = !string.IsNullOrEmpty(options.Domain);
+                bool pathHasValue = !string.IsNullOrEmpty(options.Path);
+                bool expiresHasValue = options.Expires.HasValue;
+
+                var cookie = new HttpCookie(key, value);
+                if (domainHasValue)
+                {
+                    cookie.Domain = options.Domain;
+                }
+                if (pathHasValue)
+                {
+                    cookie.Path = options.Path;
+                }
+                if (expiresHasValue)
+                {
+                    cookie.Expires = options.Expires.Value;
+                }
+                if (options.Secure)
+                {
+                    cookie.Secure = true;
+                }
+                if (options.HttpOnly)
+                {
+                    cookie.HttpOnly = true;
+                }
+
+                webContext.Response.AppendCookie(cookie);
+            }
+
+            public void DeleteCookie(IOwinContext context, string key, Microsoft.Owin.CookieOptions options)
+            {
+                if (context == null)
+                {
+                    throw new ArgumentNullException("context");
+                }
+                if (options == null)
+                {
+                    throw new ArgumentNullException("options");
+                }
+
+                AppendResponseCookie(
+                    context,
+                    key,
+                    string.Empty,
+                    new Microsoft.Owin.CookieOptions
+                    {
+                        Path = options.Path,
+                        Domain = options.Domain,
+                        Expires = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc),
+                    });
+            }
+
+            public RequestCookieCollection GetCookiesNames(IOwinContext context)
+            {
+                if (context == null)
+                {
+                    throw new ArgumentNullException("context");
+                }
+
+                var webContext = context.Get<HttpContextBase>(typeof(HttpContextBase).FullName);
+
+                var coockies = webContext.Response.Cookies;
+                var dicc = new Dictionary<string, string>();
+                foreach (var cookieName in coockies.AllKeys)
+                {
+                    dicc.Add(cookieName, coockies[cookieName].Value);
+                }
+
+                return new RequestCookieCollection(dicc);
+            }
+        }
+
+        public static IAppBuilder ConfigureCookieAuthentication(this IAppBuilder app, IdentityServer3.Core.Configuration.CookieOptions options, IDataProtector dataProtector)
         {
             if (options == null) throw new ArgumentNullException("options");
             if (dataProtector == null) throw new ArgumentNullException("dataProtector");
@@ -40,8 +144,10 @@ namespace Owin
                 options.Prefix += ".";
             }
 
+            var cookieManager = new SystemWebIdSrvCookieManager();
             var primary = new CookieAuthenticationOptions
             {
+                CookieManager = cookieManager,
                 AuthenticationType = Constants.PrimaryAuthenticationType,
                 CookieName = options.Prefix + Constants.PrimaryAuthenticationType,
                 ExpireTimeSpan = options.ExpireTimeSpan,
@@ -66,6 +172,7 @@ namespace Owin
 
             var external = new CookieAuthenticationOptions
             {
+                CookieManager = cookieManager,
                 AuthenticationType = Constants.ExternalAuthenticationType,
                 CookieName = options.Prefix + Constants.ExternalAuthenticationType,
                 AuthenticationMode = AuthenticationMode.Passive,
@@ -78,6 +185,7 @@ namespace Owin
 
             var partial = new CookieAuthenticationOptions
             {
+                CookieManager = cookieManager,
                 AuthenticationType = Constants.PartialSignInAuthenticationType,
                 CookieName = options.Prefix + Constants.PartialSignInAuthenticationType,
                 AuthenticationMode = AuthenticationMode.Passive,
